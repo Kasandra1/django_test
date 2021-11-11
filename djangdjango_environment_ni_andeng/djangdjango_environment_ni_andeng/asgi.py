78fd8fd7287028737934a1016886450c5491487f
DESKTOP-GEQ29NM\Michael <michael.anthony.basanta@gmail.com>
2021-11-11 15:27:55 +0800

    additional changes in gitignore

---
+++ djangdjango_environment_ni_andeng/djangdjango_environment_ni_andeng/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for djangdjango_environment_ni_andeng project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangdjango_environment_ni_andeng.settings')
+
+application = get_asgi_application()
